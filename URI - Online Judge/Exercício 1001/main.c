59f2def1fdf792bbae82bd7ce9de3eb1c9810ec6
DefensorDaHumanidade <alexandre.fava@hotmail.com>
2019-01-03 10:50:45 -0200

    Enviando Exercício Número 1001

---
+++ URI - Online Judge/Exercício 1001/main.c
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    
+    short int A, B, X; //DECLARAÇÃO DAS VARIÁVEIS A, B, X
+    
+    cin >> A; //ATRIBUINDO VALORES PARA A
+    cin >> B; //ATRIBUINDO VALORES PARA B
+    
+    X = A+B; //CARREGANDO A SOMA DE A+B NA VARIÁVEL X
+    
+    cout << "X = " << X <<endl; //IMPRIMINDO EM TELA O VALOR DE X
+ 
+    return 0;
+}
+
+
+//PRIMEIRO EXERCÍCIO DO 'URI - ONLINE JUDGE', PROBLEMA NÚMERO: 1001
