ce0b7f5ad8fbd35180f63caef3971e2ace418465
DefensorDaHumanidade <alexandre.fava@hotmail.com>
2019-01-26 04:17:24 -0200

    Arrumando indentações

--- Iniciante/Exercício 1012/main.cpp
+++ Iniciante/Exercício 1012/main.cpp
@@ -12,10 +12,10 @@ int main(){
 	cin >> A >> B >> C; //ATRIBUINDO VALORES PARA AS VARIÁVEIS DECLARADAS
 
 	cout << "TRIANGULO: " << fixed << setprecision(3) << A*C/2.0 		<< endl; //IMPRIMINDO EM TELA O VALOR DA ÁREA DO TRIÂNGULO
-	cout << "CIRCULO: "   << fixed << setprecision(3) << PI*pow(C, 2.0) << endl; //IMPRIMINDO EM TELA O VALOR DA ÁREA DA CIRCUNFERÊNCIA
+	cout << "CIRCULO: "   << fixed << setprecision(3) << PI*pow(C, 2.0) 	<< endl; //IMPRIMINDO EM TELA O VALOR DA ÁREA DA CIRCUNFERÊNCIA
 	cout << "TRAPEZIO: "  << fixed << setprecision(3) << (A+B)*C/2.0 	<< endl; //IMPRIMINDO EM TELA O VALOR DA ÁREA DO TRAPÉZIO
 	cout << "QUADRADO: "  << fixed << setprecision(3) << pow(B, 2.0) 	<< endl; //IMPRIMINDO EM TELA O VALOR DA ÁREA DO QUADRADO
-	cout << "RETANGULO: " << fixed << setprecision(3) << A*B			<< endl; //IMPRIMINDO EM TELA O VALOR DA ÁREA DO RETÂNGULO
+	cout << "RETANGULO: " << fixed << setprecision(3) << A*B		<< endl; //IMPRIMINDO EM TELA O VALOR DA ÁREA DO RETÂNGULO
 
 	return 0;
 }
