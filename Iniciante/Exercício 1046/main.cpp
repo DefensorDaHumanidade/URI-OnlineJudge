ec9e060ad3079e40180edf09168599789bb7495a
Alexandre <alexandre.fava@hotmail.com>
2019-09-03 12:07:47 -0300

    Enviando arquivo .cpp: 'Tempo de Jogo'

---
+++ Iniciante/Exercício 1046/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	short int HoraInicial, HoraFinal; //DECLARAÇÃO DAS VARIÁVEIS
+
+	cin >> HoraInicial >> HoraFinal;
+
+	if( HoraInicial == HoraFinal)		cout << "O JOGO DUROU 24 HORA(S)" << endl;
+	if( HoraInicial > HoraFinal)		cout << "O JOGO DUROU " << 24 - HoraInicial + HoraFinal << " HORA(S)" << endl;
+	if( HoraInicial < HoraFinal)		cout << "O JOGO DUROU " << HoraFinal - HoraInicial << " HORA(S)" << endl;
+
+	return 0;
+}
+
+//EXERCÍCIO DO 'URI - ONLINE JUDGE', PROBLEMA NÚMERO: 1046
