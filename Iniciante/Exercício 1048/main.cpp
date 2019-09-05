7aa8fea709e359886a207def8d5d56df086165ca
Alexandre <alexandre.fava@hotmail.com>
2019-09-05 10:53:41 -0300

    Enviando arquivo .cpp: 'Aumento de Salário'

---
+++ Iniciante/Exercício 1048/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iomanip> //BIBLIOTECA DO 'setprecision'
+
+using namespace std;
+
+int main(){
+
+	double salario; //DECLARAÇÃO DA VARIÁVEL 'salario'
+
+	cin >> salario; //INFORMANDO O SALARIO DO FUNCIONÁRIO
+
+    if( salario <= 400 )                    cout << "Novo salario: " << fixed << setprecision(2) << salario+salario*0.15 << endl << "Reajuste ganho: " << salario*0.15 << endl << "Em percentual: 15 \%" << endl;
+    if( salario > 400 && salario <= 800)    cout << "Novo salario: " << fixed << setprecision(2) << salario+salario*0.12 << endl << "Reajuste ganho: " << salario*0.12 << endl << "Em percentual: 12 \%" << endl;
+    if( salario > 800 && salario <= 1200)   cout << "Novo salario: " << fixed << setprecision(2) << salario+salario*0.10 << endl << "Reajuste ganho: " << salario*0.10 << endl << "Em percentual: 10 \%" << endl;
+    if( salario > 1200 && salario <= 2000)  cout << "Novo salario: " << fixed << setprecision(2) << salario+salario*0.07 << endl << "Reajuste ganho: " << salario*0.07 << endl << "Em percentual: 7 \%" << endl;
+    if( salario > 2000)                     cout << "Novo salario: " << fixed << setprecision(2) << salario+salario*0.04 << endl << "Reajuste ganho: " << salario*0.04 << endl << "Em percentual: 4 \%" << endl;
+
+    return 0;
+}
+
+//EXERCÍCIO DO 'URI - ONLINE JUDGE', PROBLEMA NÚMERO: 1048
